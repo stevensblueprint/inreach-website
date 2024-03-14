5442e368baa76f004ce8cb8deb80a14294be2417
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-03-14 15:35:55 -0400

    change default port to match next.js for vercel deploy

--- tina/config.ts
+++ tina/config.ts
@@ -9,7 +9,7 @@ const branch = process.env.GITHUB_BRANCH ||
     const getBaseUrl = () => {
 	if (typeof window !== 'undefined') return '' // browser should use relative url
 	// if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
-	return `http://localhost:${process.env.PORT ?? 8000}` // dev SSR should use localhost
+	return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
 }
 export default defineConfig({
     contentApiUrlOverride: `${getBaseUrl()}/api/tina/gql`,
