569f5ef77c81c3216fe9ce2ded5ca7aa3469168b
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-03-14 15:28:59 -0400

    try different base url

--- tina/config.ts
+++ tina/config.ts
@@ -8,7 +8,7 @@ const branch = process.env.GITHUB_BRANCH ||
     "main";
     const getBaseUrl = () => {
 	if (typeof window !== 'undefined') return '' // browser should use relative url
-	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
+	// if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
 	return `http://localhost:${process.env.PORT ?? 8000}` // dev SSR should use localhost
 }
 export default defineConfig({
