75907fab336dacfc0b0987c16dbfc29adf7593f3
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-03-14 17:30:11 -0400

    config

--- tina/config.ts
+++ tina/config.ts
@@ -21,6 +21,8 @@ export default defineConfig({
         outputFolder: "admin",
         publicFolder: "public",
     },
+    clientId: '123',
+    token: '456',
     media: {
         tina: {
             mediaRoot: "",
