17a9573ed99369b63caabf3f922d94b6289e45c7
Justin Ferber <ferberjustin5@gmail.com>
2024-04-17 23:16:41 -0400

    For some reason, the color only works with the inputClasses present in the colorSelector file. If I get rid of it, it doesn't work, even though it isn't used.

--- src/components/fields/colorSelector.tsx
+++ src/components/fields/colorSelector.tsx
@@ -1,6 +1,6 @@
 import { cn } from '../../lib/utils'
 import * as React from 'react'
-import { wrapFieldsWithMeta } from 'tinacms'
+import { wrapFieldsWithMeta,  } from 'tinacms'
 
 const colorOptions = [
 	'Ally Green',
@@ -24,6 +24,26 @@ const colorOptions = [
 ]
 
 export const colorSelector = wrapFieldsWithMeta(({ input }) => {
+	const inputClasses: Record<string, string> = {
+		'Ally Green': 'bg-inreach-ally-green',
+		'Light Grey': 'bg-inreach-light-grey',
+		'Soft Black': 'bg-inreach-soft-black',
+		'Dark Grey': 'bg-inreach-dark-grey',
+		'White': 'bg-inreach-white',
+		'Teal': 'bg-inreach-teal',
+		'Cornflower': 'bg-inreach-cornflower',
+		'Cool Grey': 'bg-inreach-cool-grey',
+		'Pink': 'bg-inreach-pink',
+		'Light Blue': 'bg-inreach-light-blue',
+		'Purple': 'bg-inreach-purple',
+		'Dark Blue': 'bg-inreach-dark-blue',
+		'Green': 'bg-inreach-green',
+		'Yellow': 'bg-inreach-yellow',
+		'Orange': 'bg-inreach-orange',
+		'Red': 'bg-inreach-red',
+		'Brown': 'bg-inreach-brown',
+		'Dark Brown': 'bg-inreach-dark-brown',
+	}
 
 	return (
 		<>
