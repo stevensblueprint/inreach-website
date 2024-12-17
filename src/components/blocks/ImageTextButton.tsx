93c0a099f2adf684021a669f5ca56a88904d2648
Miguel Merlin <106508795+miguel-merlin@users.noreply.github.com>
2024-12-17 00:06:27 -0500

    Fixed width Image Text Button Component (#110)
    ## Description
    Fixed width of Image Text Button Component to match provided image in
    issue
    
    <img width="1624" alt="image"
    src="https://github.com/user-attachments/assets/1cd0699b-c096-41a3-909e-63b2dbbb2bc2"
    />
    
    Solves #102

--- src/components/blocks/ImageTextButton.tsx
+++ src/components/blocks/ImageTextButton.tsx
@@ -12,25 +12,23 @@ import { cn } from '../../lib/utils'
 
 export const ImageTextButtonBlock = ({ data }: { data: PageBlocksImagetextbutton }) => {
 	return (
-		<div className='grid grid-cols-6 gap-4'>
-			<div className='col-start-3 col-span-2'>
-				<div className='flex flex-row items-center justify-center'>
-					<div className='flex-shrink-0 mr-6'>
-						<Icon icon='rivet-icons:question-mark' width={60} height={60} />
-					</div>
-					<div className='prose flex-grow max-w-full px-4'>
+		<div className='flex justify-center items-center min-h-full w-full'>
+			<div className='flex flex-row items-center'>
+				<div className='flex-shrink-0 mr-6'>
+					<Icon icon='rivet-icons:question-mark' width={60} height={60} />
+				</div>
+				<div className='flex flex-row items-center'>
+					<div className='prose mr-4'>
 						<TinaMarkdown components={tinaMarkdownComponents} content={data.blockContent || ''} />
 					</div>
-					<div className='flex-shrink-0'>
-						<button
-							className={cn(
-								'text-white font-bold py-2 px-4 rounded',
-								data.buttonBgColor ? inputClasses[data.buttonBgColor] : inputClasses['Ally Green']
-							)}
-						>
-							{data.buttonText}
-						</button>
-					</div>
+					<button
+						className={cn(
+							'text-white font-bold py-2 px-4 rounded whitespace-nowrap',
+							data.buttonBgColor ? inputClasses[data.buttonBgColor] : inputClasses['Ally Green']
+						)}
+					>
+						{data.buttonText}
+					</button>
 				</div>
 			</div>
 		</div>
